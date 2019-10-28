50844bee956bd36766d9aa623f83bb61442c7f8d
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-28 04:15:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
 class Student < User
-
+  def initialize (first_name ,last_name )
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge =[]
+  end
+  def learn(KNOWLEDGE)
+    @knowledge << KNOWLEDGE
+  end 
+  def KNOWLEDGE
+    @knowledge
 end
